9131593eaa335d275709ff7eef52d159792108c0
dheemanthreddy <sankudheemanthreddy@gmail.com>
2021-11-11 23:55:22 -0600

    update

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -37,8 +37,24 @@ exports.icecream_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: icecream delete DELETE ' + req.params.id);
 };
 // Handle icecream update form on PUT.
-exports.icecream_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: icecream update PUT' + req.params.id);
+exports.icecream_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body 
+    ${JSON.stringify(req.body)}`) 
+        try { 
+            let toUpdate = await icecream.findById( req.params.id) 
+            // Do updates of properties 
+            if(req.body.icecream_type)  
+                   toUpdate.icecream_type = req.body.icecream_type; 
+            if(req.body.cost) toUpdate.cost = req.body.cost; 
+            if(req.body.quantity) toUpdate.quantity = req.body.quantity; 
+            let result = await toUpdate.save(); 
+            console.log("Sucess " + result) 
+            res.send(result) 
+        } catch (err) { 
+            res.status(500) 
+            res.send(`{"error": ${err}: Update for id ${req.params.id} 
+    failed`); 
+        } 
 }; 
 // VIEWS
 // Handle a show all view
