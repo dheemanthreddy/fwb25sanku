bda257751991f9c146c09ec9772b27b06525d096
dheemanthreddy <sankudheemanthreddy@gmail.com>
2021-11-17 18:02:25 -0600

    created page view to delete create and update collections

--- routes/icecream.js
+++ routes/icecream.js
@@ -2,7 +2,20 @@ var express = require('express');
 const icecream_controlers= require('../controllers/icecream')
 var router = express.Router();
 
-/* GET pens */
+/* GET icecream */
 router.get('/', icecream_controlers.icecream_view_all_Page);
+
+/* GET detail icecream page */ 
+router.get('/detail', icecream_controlers.icecream_view_one_Page); 
+
+/* GET create icecream page */ 
+router.get('/create', icecream_controlers.icecream_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', icecream_controlers.icecream_update_Page); 
+
+/* GET create icecream page */ 
+router.get('/delete', icecream_controlers.icecream_delete_Page); 
+ 
 
 module.exports = router;
