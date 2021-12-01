f56d4751cef49687007fb11734a14c82136b05eb
dheemanthreddy <sankudheemanthreddy@gmail.com>
2021-12-01 15:42:10 -0600

    commite

--- routes/icecream.js
+++ routes/icecream.js
@@ -11,8 +11,20 @@ router.get('/detail', icecream_controlers.icecream_view_one_Page);
 /* GET create icecream page */ 
 router.get('/create', icecream_controlers.icecream_create_Page); 
 
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
+
+
 /* GET create update page */ 
-router.get('/update', icecream_controlers.icecream_update_Page); 
+router.get('/update',secured , icecream_controlers.icecream_update_Page);
 
 /* GET create icecream page */ 
 router.get('/delete', icecream_controlers.icecream_delete_Page); 
