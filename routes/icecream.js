2c8e762d1cbea901e122fd45062ae36b86c6399b
dheemanthreddy <sankudheemanthreddy@gmail.com>
2021-12-01 21:19:50 -0600

    bonus

--- routes/icecream.js
+++ routes/icecream.js
@@ -2,15 +2,6 @@ var express = require('express');
 const icecream_controlers= require('../controllers/icecream')
 var router = express.Router();
 
-/* GET icecream */
-router.get('/', icecream_controlers.icecream_view_all_Page);
-
-/* GET detail icecream page */ 
-router.get('/detail', icecream_controlers.icecream_view_one_Page); 
-
-/* GET create icecream page */ 
-router.get('/create', icecream_controlers.icecream_create_Page); 
-
 // A little function to check if we have an authorized user and continue on
 // or
 // redirect to login.
@@ -23,11 +14,21 @@ const secured = (req, res, next) => {
     }
 
 
+/* GET icecream */
+router.get('/', icecream_controlers.icecream_view_all_Page);
+
+/* GET detail icecream page */ 
+router.get('/detail', icecream_controlers.icecream_view_one_Page); 
+
+/* GET create icecream page */ 
+router.get('/create',secured, icecream_controlers.icecream_create_Page); 
+
+
 /* GET create update page */ 
 router.get('/update',secured , icecream_controlers.icecream_update_Page);
 
 /* GET create icecream page */ 
-router.get('/delete', icecream_controlers.icecream_delete_Page); 
+router.get('/delete',secured,icecream_controlers.icecream_delete_Page); 
  
 
 module.exports = router;
